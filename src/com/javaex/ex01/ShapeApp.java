f11cc982fdc29545d800d9d000aa4af90fc5d3fe
dasom <dasom589@gmail.com>
2024-01-14 22:17:35 +0900

    2024-01-14

--- src/com/javaex/ex01/ShapeApp.java
+++ src/com/javaex/ex01/ShapeApp.java
@@ -4,7 +4,7 @@ public class ShapeApp {
 
 	public static void main(String[] args) {
 
-		Shape s = new Ractangle("빨강", 12, 10);
+		Shape s = new Shape();
 		Circle c1 = new Circle("녹색", 10);
 		Ractangle r1 = new Ractangle("빨강", 4, 4);
 		Shape sr1 = new Ractangle("빨강", 4, 4);
